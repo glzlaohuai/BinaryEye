df7a0c4c4fc2054789beedca75cfc449f3fe8818
laohuaizzz <494763134@qq.com>
2023-07-12 14:41:23 +0800

    .

--- app/src/main/kotlin/com/awesomeapp/xscaner/app/ScannerApplication.kt
+++ app/src/main/kotlin/com/awesomeapp/xscaner/app/ScannerApplication.kt
@@ -8,6 +8,7 @@ import com.ap.android.trunk.sdk.core.APSDK
 import com.ap.android.trunk.sdk.core.others.APAdError
 import com.ap.android.trunk.sdk.core.utils.APSDKListener
 import com.ap.android.trunk.sdk.debug.activity.APADDebugActivity
+import com.awesomeapp.xscaner.ads.ID
 import com.awesomeapp.xscaner.database.Database
 import com.awesomeapp.xscaner.preference.Preferences
 
@@ -20,7 +21,7 @@ class ScannerApplication : Application(), APSDKListener {
         db.open(this)
         prefs.init(this)
 
-        APSDK.init(this, "44901c5e-1e78-4103-9c89-a179b3ba254d", this)
+        APSDK.init(this, ID.APP_ID, this)
 
         Handler(Looper.getMainLooper()).postDelayed({
             var intent = Intent(this@ScannerApplication, APADDebugActivity::class.java)
