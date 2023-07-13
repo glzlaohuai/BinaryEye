a92d3b28e17bd1ccfa2639df8a4037f002b9abc7
laohuaizzz <494763134@qq.com>
2023-07-13 10:05:46 +0800

    去掉delay gotoDebug的调用

--- app/src/main/kotlin/com/awesomeapp/xscaner/app/ScannerApplication.kt
+++ app/src/main/kotlin/com/awesomeapp/xscaner/app/ScannerApplication.kt
@@ -1,13 +1,9 @@
 package com.awesomeapp.xscaner.app
 
 import android.app.Application
-import android.content.Intent
-import android.os.Handler
-import android.os.Looper
 import com.ap.android.trunk.sdk.core.APSDK
 import com.ap.android.trunk.sdk.core.others.APAdError
 import com.ap.android.trunk.sdk.core.utils.APSDKListener
-import com.ap.android.trunk.sdk.debug.activity.APADDebugActivity
 import com.awesomeapp.xscaner.ads.ID
 import com.awesomeapp.xscaner.database.Database
 import com.awesomeapp.xscaner.preference.Preferences
@@ -22,12 +18,6 @@ class ScannerApplication : Application(), APSDKListener {
         prefs.init(this)
 
         APSDK.init(this, ID.APP_ID, this)
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            var intent = Intent(this@ScannerApplication, APADDebugActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            this@ScannerApplication.startActivity(intent)
-        }, 5000)
     }
 
 
